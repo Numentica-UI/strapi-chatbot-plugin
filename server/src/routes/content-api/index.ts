c1e06cef013ad8115ec5fc6c3e3c2e0295517a0f
joe-freddie-numenticaUI <joe.freddie@numentica-ui.com>
2026-06-23 10:43:55 +0530

    chore(api): secured and removed unwanted api endpoints

--- server/src/routes/content-api/index.ts
+++ server/src/routes/content-api/index.ts
@@ -2,28 +2,14 @@ export default () => ({
   type: "content-api",
   routes: [
     {
-      method: "GET",
-      path: "/",
-      handler: "controller.index",
-      config: {
-        auth: false,
-      },
-    },
-    {
       method: "POST",
       path: "/ask",
       handler: "ask.ask",
-      config: {
-        auth: false,
-      },
     },
     {
       method: "GET",
       path: "/suggestions-and-privacy",
       handler: "suggestionsAndPrivacy.getSuggestionsAndPrivacy",
-      config: {
-        auth: false,
-      },
     },
   ],
 });
